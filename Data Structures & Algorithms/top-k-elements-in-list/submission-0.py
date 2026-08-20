6ed6244aeb110b049f87028a12265cef8f17e9c6
RenjianHe <A18923847245@gmail.com>
2026-08-20 12:17:28 +0800

    Delete Data Structures & Algorithms/top-k-elements-in-list/submission-0.py

--- Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        for num in nums:
-            count[num] = 1 + count.get(num,0)
-        res = heapq.nlargest(k,count.keys(),key=count.get)
-        return res
